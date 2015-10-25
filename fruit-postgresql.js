67ce96726ae3934c5b6e74a0c93898dd2cc25346
Khalid REHIOUI <Rehioui@gmail.com>
2015-10-25 10:56:12 +0000

    Connect and Config methods

--- fruit-postgresql.js
+++ fruit-postgresql.js
@@ -1,3 +1,48 @@
 /* بسم الله الرحمن الرحيم */
 
-module.exports = {};
+module.exports = (function () {
+  
+  var pg      = require('pg')
+    , config  = {};
+
+  // @todo : needs to be called only one time
+  function confStringify () {
+    return 'postrgre://' 
+      + config.user + ':'
+      + config.password + '@'
+      + config.host + '/'
+      + config.database;
+  }
+  
+  function exec (query, callBack) {
+    pg.connect(confStringify(), function(err, client, done) {
+      if(err) return callBack(err);
+      client.query(query, function (err, results) {
+        client.end(); 
+        done();
+        callBack(err, results);
+      })
+    });
+  }
+  
+  function DataManager () {
+    
+    this.type = 'postgresql';
+    
+    this.connect = function (conf, callBack) {
+      config = conf;
+      exec('SELECT 1 + 1 AS solution', function (err) {
+        callBack(err);
+      })
+      return this;
+    }
+    
+    this.config = function (conf) {
+      config = conf;
+      return this;
+    }
+    
+  }
+  
+  return new DataManager;
+}());
