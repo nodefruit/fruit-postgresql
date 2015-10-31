26907679a0c9cc59ae06c37f68e729aeb1862529
Khalid REHIOUI <Rehioui@gmail.com>
2015-10-31 21:01:32 +0000

    Create and drop table tests

--- test/test.js
+++ test/test.js
@@ -16,4 +16,63 @@ describe('Connexion to the database', function () {
   it('should get connected', function () {
     assert.equal(success, true);
   });
-})
+});
+
+describe('Creating a table for test', function () {
+  var pg      = require('pg')
+    , success = false
+    , query   = 'CREATE table IF NOT EXISTS ' + tableName
+      + ' (id serial NOT NULL, name varchar(50), age integer, PRIMARY KEY (id) );'
+
+  beforeEach(function (done) {
+    var confString = 'postrgre://'
+      + config.user + ':'
+      + config.password + '@'
+      + config.host + '/'
+      + config.database;
+    
+    pg.connect(confString, function(err, client, pgdone) {
+      if(err) return done();
+      client.query(query, function (err, results) {
+        success = !err;
+        client.end(); 
+        pgdone();
+        done();
+      });
+    });
+  });
+
+  it('should create the table successfully', function () {
+    assert.equal(success, true);
+  });
+});
+
+
+
+describe('Creating a table for test', function () {
+  var pg      = require('pg')
+    , success = false
+    , query   = 'DROP TABLE ' + tableName;
+
+  beforeEach(function (done) {
+    var confString = 'postrgre://'
+      + config.user + ':'
+      + config.password + '@'
+      + config.host + '/'
+      + config.database;
+    
+    pg.connect(confString, function(err, client, pgdone) {
+      if(err) return done();
+      client.query(query, function (err, results) {
+        success = !err;
+        client.end(); 
+        pgdone();
+        done();
+      });
+    });
+  });
+
+  it('should drop the table successfully', function () {
+    assert.equal(success, true);
+  });
+});
