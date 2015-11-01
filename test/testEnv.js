be3ec42dfc0fdb21f8513c75175e1020481ea069
Khalid REHIOUI <Rehioui@gmail.com>
2015-11-01 14:47:01 +0000

    setting up travis configuration

--- test/testEnv.js
+++ test/testEnv.js
@@ -8,11 +8,10 @@ if(process.env.HOST && process.env.DB && process.env.USER && process.env.PASS!=u
     , password            : process.env.PASS
   }
 } else {
-  console.log(' You need to specify database information as environment variables ');
   console.log(' Make sure to specify a new and empty database for test            ');
   console.log(' example :                                                         ');
   console.log(' HOST="localhost" DB="test" USER="root" PASS="123" npm test        ');
-  process.exit();
+  throw new TypeError(' You need to specify database information as environment variables ');
 }
 
 module.exports = config;
